736f311fe898aae6de65aa0062eb3eeb4f766106
John Kassebaum <jak@KassebaumEngineering.com>
2025-09-13 21:39:47 -0400

    fix: Add type tag to language object for tree-sitter validation
    - Add LANGUAGE_TYPE_TAG with standard tree-sitter hash values
    - Apply type tag to External language object for validation
    - This fixes 'Invalid language object' error in tree-sitter v0.25.0

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,13 +4,18 @@ typedef struct TSLanguage TSLanguage;
 
 extern "C" TSLanguage *tree_sitter_dart();
 
-// "tree-sitter", "language" hashed
+// "tree-sitter", "language" hashed with BLAKE2
+const napi_type_tag LANGUAGE_TYPE_TAG = {
+  0x8AF2E5212AD58ABF, 0xD5006CAD83ABBA16
+};
+
 namespace {
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports["name"] = Napi::String::New(env, "dart");
-    auto *language = tree_sitter_dart();
-    exports["language"] = Napi::External<TSLanguage>::New(env, language);
+    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_dart());
+    language.TypeTag(&LANGUAGE_TYPE_TAG);
+    exports["language"] = language;
     return exports;
 }
 
